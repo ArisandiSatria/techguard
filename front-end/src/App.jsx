943569161d240e79ba8d231039eb9f1550d30861
Fatih <fatihabdurrahman03@gmail.com>
2024-01-08 23:17:15 +0700

    add login page and sign up

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,10 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />/
+      <Route path="/register" element={<Register />} />/
     </Routes>
   );
 }
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,41 @@
+import React, { useState } from "react";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
   return (
     <>
-      <p>Testing</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlfor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+          id="email"
+          name="email"
+        />
+        <label htmlfor="Password">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="Password"
+          placeholder="Enter your password"
+          id="password"
+          name="password"
+        />
+        <button type="submit">login</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Dont have an account? Register here.
+      </button>
     </>
   );
 }

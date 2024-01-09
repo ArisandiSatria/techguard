09fd206137ce132595e4483d3171f18c07826307
Fatih <fatihabdurrahman03@gmail.com>
2024-01-09 20:50:57 +0700

    Checking Login pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./components/NotFound";
+import { Register } from "./pages/Register";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />/
       <Route path="/login" element={<Login />} />/
+      <Route path="/register" element={<Register />} />/
       <Route path="*" element={<NotFound />} />
     </Routes>
   );

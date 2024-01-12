5a55c176b0e29e33ad0c5db3704c476d22834ef4
Arisandi <arisandisatria541@gmail.com>
2024-01-12 17:03:28 +0700

    add builds and routes to vercel

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-export const PORT = 3000
-export const MONGODB_URL = process.env.DB_CONN
-export const JWT_TOKEN = process.env.JWT_SECRET
-export const USER_EMAIL = process.env.USER_EMAIL
-export const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL
+export const PORT = 3000;
+export const MONGODB_URL = process.env.DB_CONN;
+export const JWT_TOKEN = process.env.JWT_SECRET;
+export const USER_EMAIL = process.env.USER_EMAIL;
+export const PASSWORD_EMAIL = process.env.PASSWORD_EMAIL;

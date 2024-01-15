a13cc8bb7eba7ac0761a744a2d95d17dab8c68f4
Arisandi <arisandisatria541@gmail.com>
2024-01-15 21:17:18 +0700

    added product panel to admin page

--- front-end/src/components/Admin/AdminProduct.jsx
+++ front-end/src/components/Admin/AdminProduct.jsx
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function AdminProduct() {
-  return <div className="admin-product-panel">AdminProduct</div>;
+  return (
+    <div className="admin-product-panel">
+      <h1>Product List</h1>
+      <div className="product-section">
+        <div className="product-card">
+          <img src="images/product-1.jpg" alt="" />
+          <p>product name</p>
+          <div className="product-card-footer">
+            <p>Rp 100.000</p>
+          </div>
+        </div>
+
+        <div className="product-card">
+          <img src="images/product-2.jpg" alt="" />
+          <p>product name</p>
+          <div className="product-card-footer">
+            <p>Rp 100.000</p>
+          </div>
+        </div>
+
+        <div className="product-card">
+          <img src="images/product-3.jpg" alt="" />
+          <p>product name</p>
+          <div className="product-card-footer">
+            <p>Rp 100.000</p>
+          </div>
+        </div>
+
+        <div className="product-card">
+          <img src="images/product-4.jpg" alt="" />
+          <p>product name</p>
+          <div className="product-card-footer">
+            <p>Rp 100.000</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

715c385af9b613dfd435e6ad6b8bc37e6ae38273
Arisandi <arisandisatria541@gmail.com>
2024-01-17 23:03:10 +0700

    added cors to backend

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors());
+
 app.listen(PORT, () => {
   console.log(`Server is running from port ${PORT}`);
 });

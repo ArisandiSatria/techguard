70293dc99188cabb9d5d58d88eeeee4980704d9c
Arisandi <arisandisatria541@gmail.com>
2024-01-20 21:55:23 +0700

    delete setting for cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -68,11 +68,7 @@ export const login = async (req, res, next) => {
     const { password: pass, ...rest } = validUser._doc;
 
     res
-      .cookie("access_token", token, {
-        secure: true,
-        domain: ".vercel.app",
-        httpOnly: false,
-      })
+      .cookie("access_token", token)
       .status(200)
       .json({ ...rest, token });
   } catch (error) {

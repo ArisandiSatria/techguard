39d861f29db559f8c13ae55909d3b357c4e2fa27
Arisandi <arisandisatria541@gmail.com>
2024-01-25 15:14:38 +0700

    added get all order functionality and UI

--- front-end/src/components/Admin/AdminOrder.jsx
+++ front-end/src/components/Admin/AdminOrder.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
 
 export default function AdminOrder() {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      setLoading(true);
+      const res = await fetch("/api/order/get-orders");
+      const data = await res.json();
+      if ((data.success = false)) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setOrders(data);
+      setLoading(false);
+    };
+
+    fetchOrders();
+  }, []);
+
+  const orderStatus = (status) => {
+    if (status == "completed") {
+      return "completed";
+    } else if (status == "processing") {
+      return "processing";
+    } else {
+      return "cancelled";
+    }
+  };
+
   return (
     <div className="admin-order-panel">
       <section>
@@ -21,27 +53,59 @@ export default function AdminOrder() {
         <div className="tbl-content">
           <table cellPadding="0" cellSpacing="0" border="0">
             <tbody>
-              <tr>
-                <td>123</td>
-                <td>001</td>
-                <td>Rp 100000000</td>
-                <td>processing</td>
-                <td className="order-detail-button">Detail</td>
-              </tr>
-              <tr>
-                <td>321</td>
-                <td>002</td>
-                <td>Rp 200000</td>
-                <td>processing</td>
-                <td className="order-detail-button">Detail</td>
-              </tr>
-              <tr>
-                <td>456</td>
-                <td>003</td>
-                <td>Rp 3500000</td>
-                <td>processing</td>
-                <td className="order-detail-button">Detail</td>
-              </tr>
+              {loading ? (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    marginTop: "10%",
+                  }}
+                >
+                  <RotatingLines
+                    visible={true}
+                    height="80"
+                    width="80"
+                    strokeColor="black"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    ariaLabel="rotating-lines-loading"
+                  />
+                </div>
+              ) : (
+                <>
+                  {orders?.map((order, index) => (
+                    <tr key={order._id + index}>
+                      <td
+                        style={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                        }}
+                      >
+                        {order._id}
+                      </td>
+                      <td
+                        style={{
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                        }}
+                      >
+                        {order.userRef}
+                      </td>
+                      <td>Rp {order.totalPrice.toLocaleString("en-US")}</td>
+                      <td
+                        className={`order-status ${orderStatus(order.status)}`}
+                      >
+                        {order.status}
+                      </td>
+                      <td className="order-detail-button">Detail</td>
+                    </tr>
+                  ))}
+                </>
+              )}
             </tbody>
           </table>
         </div>
